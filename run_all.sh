ede3a13fa20d2198d10621bdc64aea20a605c11a
Philippe Remy <premy.enseirb@gmail.com>
2020-09-07 21:49:29 +0900

    update run all

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 python -u sdenet/train/resnet_train.py --task mnist
 python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset mnist --out_dataset svhn
 
@@ -10,4 +12,4 @@ python -u sdenet/train/resnet_train.py --task svhn
 python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset svhn --out_dataset cifar10
 
 python -u sdenet/train/sdenet_train.py --task svhn
-python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network sdenet --dataset svhn --out_dataset cifar10
+python -u sdenet/eval/eval_detection.py --pre_trained_net save_sdenet_mnist/final_model.h5 --network sdenet --dataset svhn --out_dataset cifar10
