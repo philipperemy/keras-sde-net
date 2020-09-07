07c36ceadabba59f71fd22d1cb32d39192f93a0a
Philippe Remy <premy.enseirb@gmail.com>
2020-09-07 18:52:30 +0900

    unbuffer

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python sdenet/train/resnet_train.py --task mnist
-python sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset mnist --out_dataset svhn
+python -u sdenet/train/resnet_train.py --task mnist
+python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset mnist --out_dataset svhn
 
-python sdenet/train/sdenet_train.py --task mnist
-python sdenet/eval/eval_detection.py --pre_trained_net save_sdenet_mnist/final_model.h5 --network sdenet --dataset mnist --out_dataset svhn
+python -u sdenet/train/sdenet_train.py --task mnist
+python -u sdenet/eval/eval_detection.py --pre_trained_net save_sdenet_mnist/final_model.h5 --network sdenet --dataset mnist --out_dataset svhn
 
-python sdenet/train/resnet_train.py --task svhn
-python sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset svhn --out_dataset cifar10
+python -u sdenet/train/resnet_train.py --task svhn
+python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset svhn --out_dataset cifar10
 
-python sdenet/train/sdenet_train.py --task svhn
-python sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network sdenet --dataset svhn --out_dataset cifar10
+python -u sdenet/train/sdenet_train.py --task svhn
+python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network sdenet --dataset svhn --out_dataset cifar10
