73911e390c8accc200f2ac39611b8d99191aa71a
Philippe Remy <premy.enseirb@gmail.com>
2020-09-08 11:30:36 +0900

    add scripts

--- run_keras.sh
+++ run_keras.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+# RESNET - MNIST
 python -u sdenet/train/resnet_train.py --task mnist
 python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset mnist --out_dataset svhn
 
+# SDENET - MNIST
 python -u sdenet/train/sdenet_train.py --task mnist
 python -u sdenet/eval/eval_detection.py --pre_trained_net save_sdenet_mnist/final_model.h5 --network sdenet --dataset mnist --out_dataset svhn
 
+# RESNET - SVHN
 python -u sdenet/train/resnet_train.py --task svhn
 python -u sdenet/eval/eval_detection.py --pre_trained_net save_resnet_mnist/final_model.h5 --network resnet --dataset svhn --out_dataset cifar10
 
+# SDENET - SVHN
 python -u sdenet/train/sdenet_train.py --task svhn
 python -u sdenet/eval/eval_detection.py --pre_trained_net save_sdenet_mnist/final_model.h5 --network sdenet --dataset svhn --out_dataset cifar10
